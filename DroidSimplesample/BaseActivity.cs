8bf6e2a15d878eebc5ce6dc25cccc6c8ba310170
Joseph Rene <tomarcd@gmail.com>
2015-07-25 16:34:00 -0400

    -Adding android sample application

---
+++ DroidSimplesample/BaseActivity.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Android.App;
+
+using  KiipSDKXamarin.Droid;
+using Android.OS;
+
+
+namespace simplesample
+{
+	
+	public class BaseActivity : Activity, Kiip.ICallback
+	{
+		private static String KIIP_TAG = "kiip_fragment_tag";
+		private KiipFragment mKiipFragment;
+
+		protected override void OnCreate(Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+
+			if (bundle != null) {
+			mKiipFragment = (KiipFragment) FragmentManager.FindFragmentByTag(KIIP_TAG);
+			} else {
+			mKiipFragment = new KiipFragment();
+			FragmentManager.BeginTransaction ().Add (mKiipFragment, KIIP_TAG).Commit();
+			}
+
+		}
+
+		protected override void OnStart ()
+		{
+			base.OnStart ();
+			Kiip.Instance.StartSession(this);
+
+		}
+
+		protected override void OnStop ()
+		{
+			base.OnStop ();
+
+
+			Kiip.Instance.EndSession(this);
+
+		}
+
+
+
+		public virtual void OnFailed (Kiip kiip, Java.Lang.Exception exception)
+		{
+
+		}
+
+		public virtual void OnFinished (Kiip kiip, Poptart poptart)
+		{
+			mKiipFragment.ShowPoptart(poptart);
+		}
+	}
+}
+
---
+++ DroidSimplesample/MainActivity.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+using  KiipSDKXamarin.Droid;
+
+namespace simplesample
+{
+	[Activity (Label = "simplesample", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : BaseActivity
+	{
+
+		EditText _MomentId;
+		EditText _MomentValue;
+		Button _ButtonMoment;
+		Button _ButtonStart;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			_MomentId = FindViewById<EditText> (Resource.Id.moment_id);
+			_MomentValue = FindViewById<EditText> (Resource.Id.moment_value);
+			_ButtonMoment = FindViewById<Button> (Resource.Id.btn_moment);
+			_ButtonStart = FindViewById<Button> (Resource.Id.btn_start);
+			
+			_ButtonStart.Click += delegate {
+				StartActivity(typeof(SecondaryActivity));
+			};
+
+			_ButtonMoment.Click += delegate {
+				String momentId = _MomentId.Text;
+				double? momentValue = _MomentValue.Text.Length > 0 ? (double?)Double.Parse(_MomentValue.Text) : null;
+
+				if (!momentValue.HasValue) {
+					Kiip.Instance.SaveMoment(momentId, this);
+				} else {
+					Kiip.Instance.SaveMoment(momentId, momentValue.Value, this);
+				}
+			};
+		}
+	}
+}
+
+
